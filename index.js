d5e23a71eca886df374dd1f8e53dc6412c4100d2
Carl <git@carlhopf.com>
2017-07-12 11:33:41 +0200

    throw

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const LEVELS = exports.levels = {
-  ERROR: 3,
-  WARN: 4,
-  NOTICE: 5,
-  INFO: 6,
-  LOG: 7,
-  DEBUG: 7, // compat
-  TRACE: 7, // compat
+	ERROR: 3,
+	WARN: 4,
+	NOTICE: 5,
+	INFO: 6,
+	LOG: 7,
+	DEBUG: 7, // compat
+	TRACE: 7, // compat
 };
 
 var level = LEVELS.LOG;
@@ -15,22 +15,22 @@ var level = LEVELS.LOG;
  */
 
 function error (...args) {
-  if (level < LEVELS.ERROR) return;
+	if (level < LEVELS.ERROR) return;
 	console.error('<3>', ...args);
 };
  
 function warn (...args) {
-  if (level < LEVELS.WARN) return;
+	if (level < LEVELS.WARN) return;
 	console.warn('<4>', ...args);
 };
 
 function info (...args) {
-  if (level < LEVELS.INFO) return;
+	if (level < LEVELS.INFO) return;
 	console.info('<6>', ...args);
 };
 
 function log (...args) {
-  if (level < LEVELS.LOG) return;
+	if (level < LEVELS.LOG) return;
 	console.log('<7>', ...args);
 };
 
@@ -42,5 +42,6 @@ exports.debug = log; // compat
 exports.trace = log; // compat
 
 exports.setLevel = function (_level) {
-  level = _level;
+	if (typeof _level !== 'number') throw new Error('level must be number');
+	level = _level;
 };
